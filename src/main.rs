547bd9440f6b93675a5cc3330d9bde43a9df44ac
jkarrer <jkarrer@protect247.com>
2024-01-08 12:21:52 -0500

    solved

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+fn narcissistic(num: u64) -> bool {
+    //  find number of digits in number
+    let stringify_num = num.to_string();
+    let len = stringify_num.len();
+
+    // separate digits
+    let num_chars = stringify_num.chars().collect::<Vec<char>>();
+
+    // loop through
+    let mut sum = 0;
+    for c in num_chars {
+        // convert char to digit
+        let n: u64 = c.to_digit(10).unwrap().into();
+
+        // expo by the len
+        let e: u64 = n.pow(len as u32);
+
+        // sum
+        sum += e
+    }
+
+    sum == num
+}
+
 fn main() {
-    println!("Hello, world!");
+    dbg!(narcissistic(153));
 }
